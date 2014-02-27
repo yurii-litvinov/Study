dde52d88dc88d586eccc13e1f480ec51c53f8fba
Yurii Litvinov <yurii.litvinov@gmail.com>
2014-02-27 18:21:08 +0400

    Example with unit tests

---
+++ UnitTests/Stack.Test/StackTest.cs
@@ -0,0 +1,47 @@
+﻿namespace StackNamespace.Test
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using StackNamespace;
+
+    [TestClass]
+    public class StackTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            stack = new Stack();
+        }
+
+        [TestMethod]
+        public void PushTest()
+        {
+            stack.Push(1);
+            Assert.IsFalse(stack.IsEmpty());
+        }
+
+        [TestMethod]
+        public void PopTest()
+        {
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Pop());
+        }
+
+        [TestMethod]
+        public void TwoElementsPopTest()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Pop());
+            Assert.AreEqual(1, stack.Pop());
+        }
+
+        [TestMethod]
+        public void PopFromEmptyStackTest()
+        {
+            stack.Pop();
+        }
+
+        private Stack stack;
+    }
+}
---
+++ UnitTests/Stack/Stack.cs
@@ -0,0 +1,69 @@
+﻿namespace StackNamespace
+{
+    /// <summary>
+    /// Stack based on references.
+    /// </summary>
+    public class Stack
+    {
+        private class StackElement
+        {
+            private int aValue;
+
+            public int Value 
+            {
+                get
+                {
+                    return aValue;
+                }
+
+                set
+                {
+                    if (value < 0)
+                    {
+                        this.aValue = 0;
+                    }
+                    else
+                    {
+                        this.aValue = value;
+                    }
+                }
+            }
+
+            public StackElement Next { get; set; }
+        }
+
+        private StackElement head = null;
+
+        /// <summary>
+        /// Push value to a stack.
+        /// </summary>
+        /// <param name="value">Value to be pushed.</param>
+        public void Push(int value)
+        {
+            var newElement = new StackElement()
+            {
+                Next = head,
+                Value = value
+            };
+
+            head = newElement;
+        }
+
+        public int Pop()
+        {
+            if (head == null)
+            {
+                return -1;
+            }
+
+            var temp = head.Value;
+            head = head.Next;
+            return temp;
+        }
+
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+    }
+}
